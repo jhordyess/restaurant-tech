a5995379137bda3948a2cdbdd054b7a10ffa7a6f
jhordyess <dev@jhordyess.com>
2023-05-23 01:07:20 +0000

    Added basis scripts

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const root = createRoot(document.getElementById("app"));
+root.render(<div>Hello World!</div>);
