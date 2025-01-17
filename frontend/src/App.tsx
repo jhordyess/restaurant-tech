5accefbacf23e57a6809327680faa58e651fb57a
jhordyess <dev@jhordyess.com>
2025-01-17 17:37:05 -0400

    refactor: Remove Redux integration and related middleware for cleaner state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,7 @@ import * as React from 'react'
 import Customer from './layouts/customer'
 import { HashRouter } from 'react-router-dom'
 import { CustomerRoutes, customersLinks } from './routes'
-import { Provider } from 'react-redux'
-import { applyMiddleware, compose as reduxCompose, legacy_createStore as createStore } from 'redux'
-import reducer from '@/store/reducer'
-import thunk from 'redux-thunk'
-import { logger } from '@/store/middleware'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || reduxCompose
-
-const enhancer = compose(applyMiddleware(thunk, logger))
-
-const store = createStore(reducer, enhancer)
 
 const queryClient = new QueryClient()
 
@@ -23,9 +12,7 @@ export default function App() {
       <HashRouter>
         <QueryClientProvider client={queryClient}>
           <Customer links={customersLinks}>
-            <Provider store={store}>
-              <CustomerRoutes />
-            </Provider>
+            <CustomerRoutes />
           </Customer>
         </QueryClientProvider>
       </HashRouter>
